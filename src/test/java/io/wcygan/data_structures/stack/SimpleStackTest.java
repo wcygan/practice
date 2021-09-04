8a9f2944c705b628deedd1b892ff8866a7008372
Will Cygan <32029716+wcygan@users.noreply.github.com>
2021-09-03 22:27:42 -0500

    added parameterized test

--- src/test/java/io/wcygan/data_structures/stack/SimpleStackTest.java
+++ src/test/java/io/wcygan/data_structures/stack/SimpleStackTest.java
@@ -3,6 +3,11 @@ package io.wcygan.data_structures.stack;
 import io.wcygan.abstract_data_types.Stack;
 import io.wcygan.data_structures.SimpleStack;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -28,5 +33,17 @@ public class SimpleStackTest {
     int i = stack.pop();
     assertEquals(0, stack.size());
     assertEquals(999, i);
+  }
+
+  @ParameterizedTest
+  @MethodSource("provideDataForAddMany")
+  public void addMany(List<Integer> toAdd) {
+    Stack<Integer> stack = new SimpleStack<>();
+    toAdd.forEach(stack::push);
+    assertEquals(toAdd.size(), stack.size());
+  }
+
+  private static Stream<List<Integer>> provideDataForAddMany() {
+    return Stream.of(List.of(1), List.of(1, 2, 3), List.of(1, 2, 3, 3, 3, 3, 3), List.of());
   }
 }
