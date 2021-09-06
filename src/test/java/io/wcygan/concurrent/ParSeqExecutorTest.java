9d1c570b7b06467e9b93465ac627168368609efb
Will Cygan <32029716+wcygan@users.noreply.github.com>
2021-09-05 21:56:37 -0500

    added parallel task test

--- src/test/java/io/wcygan/concurrent/ParSeqExecutorTest.java
+++ src/test/java/io/wcygan/concurrent/ParSeqExecutorTest.java
@@ -16,4 +16,15 @@ public class ParSeqExecutorTest extends ParSeqTestBase {
     Assertions.assertEquals(
         Stream.of(2, 5).sorted().toList(), runAndWait(a).stream().sorted().toList());
   }
+
+  @Test
+  public void testPar() {
+    Task<?> par =
+        Task.par(
+            Task.callable(() -> PrimeFactorization.primeFactors(920)),
+            Task.callable(() -> PrimeFactorization.primeFactors(242406)),
+            Task.callable(() -> PrimeFactorization.primeFactors(992922)),
+            Task.callable(() -> PrimeFactorization.primeFactors(4567822)));
+    Assertions.assertNotNull(runAndWait(par));
+  }
 }
