1caca4d7ec060c32776307b65e6b0265aa48476c
Will Cygan <32029716+wcygan@users.noreply.github.com>
2021-09-10 00:20:09 -0500

    added NonblockingRandom

---
+++ src/test/java/io/wcygan/concurrent/nonblocking/NonblockingRandomTest.java
@@ -0,0 +1,20 @@
+package io.wcygan.concurrent.nonblocking;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NonblockingRandomTest {
+
+  private static final int LIMIT = 10000;
+
+  @Test
+  public void testRandom() {
+    NonblockingRandom random = new NonblockingRandom(System.nanoTime());
+    long curr = random.nextLong(System.nanoTime());
+    for (int i = 0; i < LIMIT; i++) {
+      long next = random.nextLong(curr);
+      Assertions.assertNotEquals(curr, next);
+      curr = next;
+    }
+  }
+}
