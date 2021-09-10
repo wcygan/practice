e1c6c2457b6ef66bd8b7ac8e4e034b660a9978b5
Will Cygan <32029716+wcygan@users.noreply.github.com>
2021-09-10 00:49:06 -0500

    concurrent stack

---
+++ src/main/java/io/wcygan/concurrent/nonblocking/ConcurrentStack.java
@@ -0,0 +1,60 @@
+package io.wcygan.concurrent.nonblocking;
+
+import io.wcygan.collections.stack.Stack;
+
+import javax.annotation.concurrent.ThreadSafe;
+import java.util.concurrent.atomic.AtomicReference;
+
+@ThreadSafe
+public class ConcurrentStack<T> implements Stack<T> {
+
+  private static final Integer FIRST_RANK = 1;
+
+  AtomicReference<Node<T>> top = new AtomicReference<>();
+
+  @Override
+  public void push(T data) {
+    Node<T> newHead = new Node<T>(data, -1);
+    Node<T> oldHead;
+    do {
+      oldHead = top.get();
+      newHead.next = oldHead;
+      newHead.rank = (oldHead == null) ? FIRST_RANK : oldHead.rank + 1;
+    } while (!top.compareAndSet(oldHead, newHead));
+  }
+
+  @Override
+  public T pop() {
+    Node<T> oldHead;
+    Node<T> newHead;
+    do {
+      oldHead = top.get();
+      if (oldHead == null) return null;
+      newHead = oldHead.next;
+    } while (!top.compareAndSet(oldHead, newHead));
+    return oldHead.item;
+  }
+
+  @Override
+  public T peek() {
+    Node<T> n = top.get();
+    return (n == null) ? null : n.item;
+  }
+
+  @Override
+  public int size() {
+    Node<T> n = top.get();
+    return (n == null) ? 0 : n.rank;
+  }
+
+  private static class Node<T> {
+    public final T item;
+    public int rank;
+    public Node<T> next;
+
+    public Node(T item, int rank) {
+      this.item = item;
+      this.rank = rank;
+    }
+  }
+}
---
+++ src/test/java/io/wcygan/concurrent/nonblocking/ConcurrentStackTest.java
@@ -0,0 +1,61 @@
+package io.wcygan.concurrent.nonblocking;
+
+import io.wcygan.collections.stack.Stack;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class ConcurrentStackTest {
+
+  private static Integer NUM_THREADS = 10;
+
+  @Test
+  public void pushThreeTimesPopThreeTimes() {
+    List<Integer> toAdd = new ArrayList<>(List.of(1, 2, 3));
+    Stack<Integer> stack = new ConcurrentStack<>();
+    Assertions.assertNull(stack.peek());
+
+    toAdd.forEach(stack::push);
+    Assertions.assertEquals(toAdd.get(toAdd.size() - 1), stack.peek());
+    Collections.reverse(toAdd);
+
+    Assertions.assertEquals(toAdd.size(), stack.size());
+    toAdd.forEach(element -> Assertions.assertEquals(element, stack.pop()));
+    Assertions.assertEquals(0, stack.size());
+    Assertions.assertNull(stack.peek());
+  }
+
+  @Test
+  public void testMultipleThreads() {
+    int range = 25;
+    Stack<Integer> stack = new ConcurrentStack<>();
+
+    Runnable pushRange = () -> {
+      IntStream.range(0, range).forEach(stack::push);
+    };
+
+    List<Thread> threads = new ArrayList<>();
+    for (int i = 0; i < NUM_THREADS; i++) {
+      Thread t = new Thread(pushRange);
+      threads.add(t);
+      t.start();
+    }
+
+    for (Thread t : threads) {
+      try {
+        t.join();
+      } catch (InterruptedException e) {
+        Assertions.fail("Failed to join thread " + t.getName());
+      }
+    }
+
+    Assertions.assertEquals(range * NUM_THREADS, stack.size());
+    Assertions.assertEquals(range - 1, stack.peek());
+  }
+
+
+}
