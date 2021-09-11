14761241d053adbd9c07f7fc400e8c543b1185e0
Will Cygan <32029716+wcygan@users.noreply.github.com>
2021-09-11 12:15:41 -0500

    added recursive sum task for fork/join framework

---
+++ src/main/java/io/wcygan/concurrent/executors/task/SumTask.java
@@ -0,0 +1,38 @@
+package io.wcygan.concurrent.executors.task;
+
+import java.util.List;
+import java.util.concurrent.RecursiveTask;
+
+/**
+ * Takes the sum of a list of integers
+ *
+ * <p>Invoking the task will result in recursively dividing it up into subtasks to be computed
+ * concurrently (or in parallel if there are enough processors available)
+ */
+public class SumTask extends RecursiveTask<Long> {
+  int low;
+  int high;
+  List<Integer> lst;
+
+  SumTask(List<Integer> lst, int low, int high) {
+    this.lst = lst;
+    this.low = low;
+    this.high = high;
+  }
+
+  protected Long compute() {
+    if (high - low <= 10) {
+      long sum = 0;
+      for (int i = low; i < high; ++i) sum += lst.get(i);
+      return sum;
+    } else {
+      int mid = low + (high - low) / 2;
+      SumTask left = new SumTask(lst, low, mid);
+      SumTask right = new SumTask(lst, mid, high);
+      left.fork();
+      long rightResult = right.compute();
+      long leftResult = left.join();
+      return leftResult + rightResult;
+    }
+  }
+}
