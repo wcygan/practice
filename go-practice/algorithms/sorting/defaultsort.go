4a5a097ad495d1df991f53813d5f3b23dfdc2a46
Will Cygan <32029716+wcygan@users.noreply.github.com>
2021-09-16 09:48:21 -0500

    nit

--- go-practice/algorithms/sorting/defaultsort.go
+++ go-practice/algorithms/sorting/defaultsort.go
@@ -4,11 +4,11 @@ import "sort"
 
 type DefaultSort struct{}
 
-func (b DefaultSort) Kind() string {
+func (d DefaultSort) Kind() string {
 	return "DefaultSort"
 }
 
-func (b DefaultSort) Sorted(arr []int) []int {
+func (d DefaultSort) Sorted(arr []int) []int {
 	clone := Clone(arr)
 	sort.Ints(clone)
 	return clone
