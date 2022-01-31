0e6d6d52884416afb4b58b0bdbe210552ca5ecce
William Cygan <32029716+wcygan@users.noreply.github.com>
2022-01-31 15:55:53 -0600

    nit

--- java-practice/src/main/java/io/wcygan/modern/PredicateUtils.java
+++ java-practice/src/main/java/io/wcygan/modern/PredicateUtils.java
@@ -6,6 +6,6 @@ import java.util.function.Predicate;
  * https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/util/function/Predicate.html
  */
 public class PredicateUtils {
-  public static final Predicate<Integer> IS_EVEN = (n) -> n % 2 == 0;
-  public static final Predicate<Integer> IS_ODD = (n) -> n % 2 != 0;
+    public static final Predicate<Integer> IS_EVEN = (n) -> n % 2 == 0;
+    public static final Predicate<Integer> IS_ODD = (n) -> n % 2 != 0;
 }
--- java-practice/src/test/java/io/wcygan/modern/VarUtilsTest.java
+++ java-practice/src/test/java/io/wcygan/modern/VarUtilsTest.java
@@ -3,17 +3,15 @@ package io.wcygan.modern;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Map;
-
 public class VarUtilsTest {
 
-  private static final String HELLO = "Hello";
-  private static final String WORLD = "World";
+    private static final String HELLO = "Hello";
+    private static final String WORLD = "World";
 
-  @Test
-  public void testCreateMap() {
-    Map<String, String> map = VarUtils.createMap(HELLO, WORLD);
-    Assertions.assertTrue(map.containsKey(HELLO));
-    Assertions.assertEquals(WORLD, map.get(HELLO));
-  }
+    @Test
+    public void testCreateMap() {
+        var map = VarUtils.createMap(HELLO, WORLD);
+        Assertions.assertTrue(map.containsKey(HELLO));
+        Assertions.assertEquals(WORLD, map.get(HELLO));
+    }
 }
