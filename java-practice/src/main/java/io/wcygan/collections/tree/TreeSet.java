df0a78054b6d2f61f5efa0da8bee0f6e1fb1802f
William Cygan <32029716+wcygan@users.noreply.github.com>
2022-02-06 14:56:36 -0600

    finished treeset

--- java-practice/src/main/java/io/wcygan/collections/tree/TreeSet.java
+++ java-practice/src/main/java/io/wcygan/collections/tree/TreeSet.java
@@ -15,12 +15,16 @@ public class TreeSet<T extends Comparable<T>> implements Set<T> {
         this.root = null;
     }
 
+    private static <T extends Comparable<T>> boolean isLessThan(T left, T right) {
+        return is(left).lessThan(right);
+    }
+
     @Override
     public boolean contains(T data) {
         return search(root, data);
     }
 
-    public boolean search(Node<T> root, T data) {
+    private boolean search(Node<T> root, T data) {
         if (Optional.ofNullable(root).isEmpty()) {
             return false;
         }
@@ -29,11 +33,8 @@ public class TreeSet<T extends Comparable<T>> implements Set<T> {
             return true;
         }
 
-        if (is(data).lessThan(root.data)) {
-            return search(root.left, data);
-        } else {
-            return search(root.right, data);
-        }
+        Node<T> next = is(data).lessThan(root.data) ? root.left : root.right;
+        return search(next, data);
     }
 
     @Override
@@ -48,7 +49,7 @@ public class TreeSet<T extends Comparable<T>> implements Set<T> {
         return insert(null, this.root, toAdd);
     }
 
-    public boolean insert(Node<T> parent, Node<T> current, Node<T> toAdd) {
+    private boolean insert(Node<T> parent, Node<T> current, Node<T> toAdd) {
         if (Optional.ofNullable(current).isEmpty()) {
             if (toAdd.isLessThan(parent)) {
                 parent.left = toAdd;
@@ -63,26 +64,142 @@ public class TreeSet<T extends Comparable<T>> implements Set<T> {
             return false;
         }
 
-        if (toAdd.isLessThan(current)) {
-            return insert(current, current.left, toAdd);
-        } else {
-            return insert(current, current.right, toAdd);
-        }
+        Node<T> next = toAdd.isLessThan(current) ? current.left : current.right;
+        return insert(current, next, toAdd);
     }
 
     @Override
     public boolean delete(T data) {
-        throw new Error("not implemented");
+        return delete(null, this.root, data);
+    }
+
+    private boolean delete(Node<T> parent, Node<T> current, T data) {
+        if (Optional.ofNullable(current).isEmpty()) {
+            return false;
+        }
+
+        if (current.data.equals(data)) {
+            return deleteNode(parent, current);
+        }
+
+        Node<T> next = isLessThan(data, current.data) ? current.left : current.right;
+        return delete(current, next, data);
+    }
+
+    private boolean deleteNode(Node<T> parent, Node<T> toDelete) {
+        int children = 0;
+        var left = Optional.ofNullable(toDelete.left);
+        var right = Optional.ofNullable(toDelete.right);
+        if (left.isPresent()) {
+            children++;
+        }
+
+        if (right.isPresent()) {
+            children++;
+        }
+
+        switch (children) {
+            case 0 -> {
+                return deleteNodeNoChildren(parent, toDelete);
+            }
+            case 1 -> {
+                return deleteNodeOneChild(parent, toDelete);
+            }
+            case 2 -> {
+                return deleteNodeTwoChildren(parent, toDelete);
+            }
+            default -> {
+                throw new Error("unreachable");
+            }
+        }
+    }
+
+    private boolean deleteNodeNoChildren(Node<T> parent, Node<T> toDelete) {
+        if (toDelete == this.root) {
+            this.root = null;
+            return true;
+        }
+
+        if (toDelete.isLessThan(parent)) {
+            parent.left = null;
+        } else {
+            parent.right = null;
+        }
+
+        return true;
+    }
+
+    private boolean deleteNodeOneChild(Node<T> parent, Node<T> toDelete) {
+        var child = Optional.ofNullable(toDelete.left).orElse(toDelete.right);
+
+        if (toDelete == this.root) {
+            this.root = child;
+
+        } else {
+            if (toDelete.isLessThan(parent)) {
+                parent.left = child;
+            } else {
+                parent.right = child;
+            }
+        }
+
+        child.parent = parent;
+        return true;
+    }
+
+    private boolean deleteNodeTwoChildren(Node<T> parent, Node<T> toDelete) {
+        var replacerNode = smallestNodeInRightSubtree(toDelete.right);
+
+        if (Optional.ofNullable(replacerNode).isEmpty()) {
+            return false;
+        }
+
+        toDelete.data = replacerNode.data;
+
+        return deleteNode(replacerNode.parent, replacerNode);
+    }
+
+    private Node<T> smallestNodeInRightSubtree(Node<T> root) {
+        if (Optional.ofNullable(root).isEmpty()) {
+            return null;
+        }
+
+        var curr = root;
+        while (Optional.ofNullable(curr.left).isPresent()) {
+            curr = curr.left;
+        }
+
+        return curr;
     }
 
     @Override
     public T minimum() {
-        throw new Error("not implemented");
+        if (root == null) {
+            return null;
+        }
+
+        var curr = root;
+
+        while (Optional.ofNullable(curr.left).isPresent()) {
+            curr = curr.left;
+        }
+
+        return curr.data;
     }
 
     @Override
     public T maximum() {
-        throw new Error("not implemented");
+        if (root == null) {
+            return null;
+        }
+
+        var curr = root;
+
+        while (Optional.ofNullable(curr.right).isPresent()) {
+            curr = curr.right;
+        }
+
+        return curr.data;
     }
 
     private static class Node<T extends Comparable<T>> {
@@ -100,7 +217,8 @@ public class TreeSet<T extends Comparable<T>> implements Set<T> {
         }
 
         public boolean isLessThan(T data) {
-            return is(this.data).lessThan(data);
+            return TreeSet.isLessThan(this.data, data);
         }
+
     }
 }
--- java-practice/src/test/java/io/wcygan/collections/set/TreeSetTest.java
+++ java-practice/src/test/java/io/wcygan/collections/set/TreeSetTest.java
@@ -7,17 +7,21 @@ import java.util.Random;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TreeSetTest {
     @Test
     public void insertHappyPath() {
         Set<Integer> s = new TreeSet<>();
+
+        assertNull(s.minimum());
+        assertNull(s.maximum());
         for (int i = 0; i < 10; i++) {
             assertTrue(s.insert(i));
             assertTrue(s.contains(i));
         }
+        assertEquals(s.minimum(), 0);
+        assertEquals(s.maximum(), 9);
     }
 
     @Test
@@ -41,13 +45,78 @@ public class TreeSetTest {
     @Test
     public void randomInsertions() {
         Random random = new Random();
-        var keys = IntStream.range(0, 25).map(i -> random.nextInt()).boxed().collect(Collectors.toList());
-        
+        var keys = IntStream.range(0, 50).map(i -> random.nextInt()).boxed().collect(Collectors.toSet());
+
+        var min = keys.stream().min(Integer::compare).get();
+        var max = keys.stream().max(Integer::compare).get();
+
         Set<Integer> s = new TreeSet<>();
         for (var key : keys) {
             assertTrue(s.insert(key));
             assertFalse(s.insert(key));
             assertTrue(s.contains(key));
+        }
+
+        assertEquals(min, s.minimum());
+        assertEquals(max, s.maximum());
+    }
+
+    @Test
+    public void insertAndDeleteSequentially() {
+        Set<Integer> s = new TreeSet<>();
+        for (int i = 0; i < 10; i++) {
+            assertTrue(s.insert(i));
+            assertTrue(s.contains(i));
+            assertTrue(s.delete(i));
+            assertFalse(s.contains(i));
+        }
+    }
+
+    @Test
+    public void insertAllThenDeleteAll() {
+        Set<Integer> s = new TreeSet<>();
+        for (int i = 0; i < 10; i++) {
+            assertTrue(s.insert(i));
+            assertTrue(s.contains(i));
+        }
+
+        for (int i = 0; i < 10; i++) {
+            assertTrue(s.delete(i));
+            assertFalse(s.contains(i));
+        }
+    }
+
+    @Test
+    public void randomInsertAndDeleteSequentially() {
+        Random random = new Random();
+        var keys = IntStream.range(0, 50).map(i -> random.nextInt()).boxed().collect(Collectors.toSet());
+
+        Set<Integer> s = new TreeSet<>();
+        for (var key : keys) {
+            assertTrue(s.insert(key));
+            assertFalse(s.insert(key));
+            assertTrue(s.contains(key));
+            assertTrue(s.delete(key));
+            assertFalse(s.contains(key));
+        }
+    }
+
+    @Test
+    public void randomInsertAllThenDeleteAll() {
+        Random random = new Random();
+        var keys = IntStream.range(0, 50).map(i -> random.nextInt(500)).boxed().collect(Collectors.toSet());
+
+        Set<Integer> s = new TreeSet<>();
+        for (var key : keys) {
+            assertTrue(s.insert(key));
+            assertFalse(s.insert(key));
+            assertTrue(s.contains(key));
+        }
+
+        for (var key : keys) {
+            assertTrue(s.contains(key));
+            assertTrue(s.delete(key));
+            assertFalse(s.contains(key));
         }
     }
 }
