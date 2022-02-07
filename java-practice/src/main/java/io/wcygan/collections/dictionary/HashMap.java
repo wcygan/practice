4ad28f076d004cb74e9d1242818c3d23cae64ce2
William Cygan <32029716+wcygan@users.noreply.github.com>
2022-02-07 13:02:25 -0600

    java: added skeleton for HashMap and TreeMap

---
+++ java-practice/src/main/java/io/wcygan/collections/dictionary/HashMap.java
@@ -0,0 +1,51 @@
+package io.wcygan.collections.dictionary;
+
+import java.util.Collection;
+import java.util.Set;
+
+public class HashMap<K extends Comparable<K>, V> implements Map<K, V> {
+    @Override
+    public int size() {
+        throw new Error("Not Implemented");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new Error("Not Implemented");
+    }
+
+    @Override
+    public boolean containsKey(Object key) {
+        throw new Error("Not Implemented");
+    }
+
+    @Override
+    public V get(Object key) {
+        throw new Error("Not Implemented");
+    }
+
+    @Override
+    public V put(K key, V value) {
+        throw new Error("Not Implemented");
+    }
+
+    @Override
+    public V remove(Object key) {
+        throw new Error("Not Implemented");
+    }
+
+    @Override
+    public void clear() {
+        throw new Error("Not Implemented");
+    }
+
+    @Override
+    public Set<K> keySet() {
+        throw new Error("Not Implemented");
+    }
+
+    @Override
+    public Collection<V> values() {
+        throw new Error("Not Implemented");
+    }
+}
