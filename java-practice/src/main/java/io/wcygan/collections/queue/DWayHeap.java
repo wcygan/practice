7e0ee7f3fd8157a1d0c11d86826958efac5d5d21
William Cygan <32029716+wcygan@users.noreply.github.com>
2022-02-11 11:43:22 -0600

    added DWayHeap

--- java-practice/src/main/java/io/wcygan/collections/queue/DWayHeap.java
+++ java-practice/src/main/java/io/wcygan/collections/queue/DWayHeap.java
@@ -2,16 +2,26 @@ package io.wcygan.collections.queue;
 
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
+import java.util.stream.IntStream;
 
-public class PriorityQueue<T> implements Queue<T> {
+public class DWayHeap<T> implements Queue<T> {
 
+    private static final Integer DEFAULT_BRANCHING_FACTOR = 3;
+
+    private final int branchingFactor;
     private final Comparator<T> comparator;
     private Object[] heap;
     private int n = 0;
     private int maxHeapSize = 16;
 
-    public PriorityQueue(Comparator<T> comparator) {
+    public DWayHeap(Comparator<T> comparator) {
+        this(comparator, DEFAULT_BRANCHING_FACTOR);
+    }
+
+    public DWayHeap(Comparator<T> comparator, int branchingFactor) {
         this.heap = new Object[maxHeapSize];
+        this.branchingFactor = branchingFactor;
         this.comparator = comparator;
     }
 
@@ -25,7 +35,7 @@ public class PriorityQueue<T> implements Queue<T> {
         n += 1;
 
         int i = n - 1;
-        while (i != 0 && comesBefore(get(i), getParent(i))) {
+        while (i != 0 && comesBefore(get(i), get(parent(i)))) {
             swap(i, parent(i));
             i = parent(i);
         }
@@ -47,23 +57,17 @@ public class PriorityQueue<T> implements Queue<T> {
         return get(0);
     }
 
-    private void growHeap() {
-        maxHeapSize *= 2;
-        heap = Arrays.copyOf(heap, maxHeapSize);
+    @SuppressWarnings("unchecked")
+    private T get(int i) {
+        return (T) heap[i];
     }
 
     private void heapify(int i) {
-        int left = leftChild(i);
-        int right = rightChild(i);
-
         int smallest = i;
-
-        if (left <= n && comesBefore(get(left), get(smallest))) {
-            smallest = left;
-        }
-
-        if (right <= n && comesBefore(get(right), get(smallest))) {
-            smallest = right;
+        for (int child : children(i)) {
+            if (child <= n && comesBefore(get(child), get(smallest))) {
+                smallest = child;
+            }
         }
 
         if (smallest != i) {
@@ -72,25 +76,20 @@ public class PriorityQueue<T> implements Queue<T> {
         }
     }
 
-    @SuppressWarnings("unchecked")
-    private T get(int i) {
-        return (T) heap[i];
+    private void growHeap() {
+        maxHeapSize *= 2;
+        heap = Arrays.copyOf(heap, maxHeapSize);
     }
 
-    private T getParent(int i) {
-        return get(parent(i));
+    private List<Integer> children(int i) {
+        return IntStream.range(1, branchingFactor + 1)
+                .map(x -> (branchingFactor * i) + x)
+                .boxed()
+                .toList();
     }
 
-    private int parent(int i) {
-        return (i - 1) / 2;
-    }
-
-    private int leftChild(int i) {
-        return 2 * i + 1;
-    }
-
-    private int rightChild(int i) {
-        return 2 * i + 2;
+    private Integer parent(int i) {
+        return (i - 1) / branchingFactor;
     }
 
     private void swap(int x, int y) {
--- java-practice/src/test/java/io/wcygan/collections/queue/PriorityQueueTest.java
+++ java-practice/src/test/java/io/wcygan/collections/queue/PriorityQueueTest.java
@@ -20,62 +20,75 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PriorityQueueTest {
     Comparator<Event> compareByInstant = Comparator.comparing(e -> e.date);
 
+    private List<Queue<Integer>> intPriorityQueueProvider() {
+        return List.of(
+                new DWayHeap<>(Integer::compare),
+                new PriorityQueue<>(Integer::compare)
+        );
+    }
+
     @Test
     public void priorityByInstant() {
-        var q = new PriorityQueue<>(compareByInstant);
-        var now = Instant.now();
-        var e1 = new Event(Date.from(now.minusSeconds(100)), 1);
-        var e2 = new Event(Date.from(now.minusSeconds(200)), 2);
-        var e3 = new Event(Date.from(now.minusSeconds(300)), 3);
-        var e4 = new Event(Date.from(now.minusSeconds(50)), 4);
-        var e5 = new Event(Date.from(now.minusSeconds(5000)), 5);
-        List.of(e3, e1, e2).forEach(q::add);
-        assertEquals(e3, q.remove());
-        assertEquals(e2, q.remove());
-        q.add(e4);
-        q.add(e5);
-        assertEquals(e5, q.remove());
-        assertEquals(e1, q.remove());
-        assertEquals(e4, q.remove());
+        List.of(
+                new PriorityQueue<>(compareByInstant),
+                new DWayHeap<>(compareByInstant)
+        ).forEach(q -> {
+            var now = Instant.now();
+            var e1 = new Event(Date.from(now.minusSeconds(100)), 1);
+            var e2 = new Event(Date.from(now.minusSeconds(200)), 2);
+            var e3 = new Event(Date.from(now.minusSeconds(300)), 3);
+            var e4 = new Event(Date.from(now.minusSeconds(50)), 4);
+            var e5 = new Event(Date.from(now.minusSeconds(5000)), 5);
+            List.of(e3, e1, e2).forEach(q::add);
+            assertEquals(e3, q.remove());
+            assertEquals(e2, q.remove());
+            q.add(e4);
+            q.add(e5);
+            assertEquals(e5, q.remove());
+            assertEquals(e1, q.remove());
+            assertEquals(e4, q.remove());
+        });
     }
 
     @Property(trials = 25)
     public void addAllRemoveAllThreeTimes(@From(IntegerListGenerator.class) List<Integer> items) {
-        var q = new PriorityQueue<>(Integer::compare);
-        var uniques = Utilities.uniques(items);
-        for (int i = 0; i < 3; i++) {
-            uniques.forEach(q::add);
-            uniques.stream().sorted(Integer::compare).forEach(x -> {
-                assertEquals(x, q.remove());
-            });
+        for (var q : intPriorityQueueProvider()) {
+            var uniques = Utilities.uniques(items);
+            for (int i = 0; i < 3; i++) {
+                uniques.forEach(q::add);
+                uniques.stream().sorted(Integer::compare).forEach(x -> {
+                    assertEquals(x, q.remove());
+                });
+            }
         }
     }
 
     @Property(trials = 25)
     public void halfAndQuarter(@From(IntegerListGenerator.class) List<Integer> items) {
         int size = items.size();
-        var q = new PriorityQueue<>(Integer::compare);
-        var uniques = Utilities.uniques(items);
-        var sorted = uniques.stream().sorted().toList();
-        uniques.forEach(q::add);
+        for (var q : intPriorityQueueProvider()) {
+            var uniques = Utilities.uniques(items);
+            var sorted = uniques.stream().sorted().toList();
+            uniques.forEach(q::add);
 
-        // remove half
-        int half = size / 2;
-        for (int i = half; i < size; i++) {
-            var want = sorted.get(i - half);
-            var got = q.remove();
-            assertEquals(want, got);
-        }
+            // remove half
+            int half = size / 2;
+            for (int i = half; i < size; i++) {
+                var want = sorted.get(i - half);
+                var got = q.remove();
+                assertEquals(want, got);
+            }
 
-        // add a quarter
-        int quarter = half / 2;
-        for (int i = 0; i < quarter; i++) {
-            q.add(sorted.get(i));
-        }
+            // add a quarter
+            int quarter = half / 2;
+            for (int i = 0; i < quarter; i++) {
+                q.add(sorted.get(i));
+            }
 
-        // remove the quarter
-        for (int i = 0; i < quarter; i++) {
-            assertEquals(sorted.get(i), q.remove());
+            // remove the quarter
+            for (int i = 0; i < quarter; i++) {
+                assertEquals(sorted.get(i), q.remove());
+            }
         }
     }
 
