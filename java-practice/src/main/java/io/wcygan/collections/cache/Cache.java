b35d995241933ea19a2253c93291b7c1025fd084
William Cygan <32029716+wcygan@users.noreply.github.com>
2022-02-12 17:14:05 -0600

    Added cache interace + LRU and LFU Cache declarations

---
+++ java-practice/src/main/java/io/wcygan/collections/cache/Cache.java
@@ -0,0 +1,15 @@
+package io.wcygan.collections.cache;
+
+import java.util.Optional;
+
+public interface Cache<Key, Value> {
+    boolean set(Key key, Value value);
+
+    Optional<Value> get(Key key);
+
+    int size();
+
+    boolean isEmpty();
+
+    void clear();
+}
---
+++ java-practice/src/main/java/io/wcygan/collections/cache/LRUCache.java
@@ -0,0 +1,30 @@
+package io.wcygan.collections.cache;
+
+import java.util.Optional;
+
+public class LRUCache<Key, Value> implements Cache<Key, Value> {
+    @Override
+    public boolean set(Key key, Value value) {
+        throw new Error("Not Implemented");
+    }
+
+    @Override
+    public Optional<Value> get(Key key) {
+        throw new Error("Not Implemented");
+    }
+
+    @Override
+    public int size() {
+        throw new Error("Not Implemented");
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new Error("Not Implemented");
+    }
+
+    @Override
+    public void clear() {
+        throw new Error("Not Implemented");
+    }
+}
