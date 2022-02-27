31b5f425222a4c1161c1f13e745ce9fe20ef0ce5
Will Cygan <wcygan.io@gmail.com>
2022-02-27 12:39:32 -0600

    Add a @main example

---
+++ scala-practice/src/main/scala/examples/MainMethod.scala
@@ -0,0 +1,11 @@
+package io.wcygan
+package examples
+
+object MainMethod {
+  /**
+   * This is an example of an entrypoint using the @main annotation
+   */
+  @main def hello(): Unit = {
+    println("Hello World")
+  }
+}
