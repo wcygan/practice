4ed5431ff4e389ce84a18d4d92230b9dfac531fc
Will Cygan <wcygan.io@gmail.com>
2022-02-27 21:22:20 -0600

    nit

--- scala-practice/src/main/scala/examples/Indentation.scala
+++ scala-practice/src/main/scala/examples/Indentation.scala
@@ -2,10 +2,14 @@ package io.wcygan
 package examples
 
 object Indentation {
+  /**
+   * To disable "significant" indentation, use the compiler flag: `-no-indent`
+   */
   @main def optional(): Unit = {
     for i <- 1 to 10 do
       if i % 2 == 0 then
         println(i)
+    end for
   }
 
   @main def braces(): Unit = {
