f021579752296f11133e609d1acf18b68e2e9a91
Will Cygan <wcygan.io@gmail.com>
2022-03-11 11:30:23 -0600

    added standard sorts and ran the benchmarks

--- java-practice/src/jmh/java/io/wcygan/algorithms/sorting/SortingBenchmark.java
+++ java-practice/src/jmh/java/io/wcygan/algorithms/sorting/SortingBenchmark.java
@@ -14,44 +14,75 @@ import java.util.stream.IntStream;
 @State(Scope.Benchmark)
 public class SortingBenchmark {
 
+    /**
+     * <pre>
+     * Benchmark                                            Mode  Cnt   Score   Error  Units
+     * i.w.a.sorting.SortingBenchmark.classicSort             ss  100  11.941 ± 0.165  ms/op
+     * i.w.a.sorting.SortingBenchmark.heapsort                ss  100  13.568 ± 0.186  ms/op
+     * i.w.a.sorting.SortingBenchmark.mergesort               ss  100  16.951 ± 0.171  ms/op
+     * i.w.a.sorting.SortingBenchmark.parallelStandardSort    ss  100   2.631 ± 0.147  ms/op
+     * i.w.a.sorting.SortingBenchmark.quicksort               ss  100  12.552 ± 0.110  ms/op
+     * i.w.a.sorting.SortingBenchmark.standardSort            ss  100  13.613 ± 0.145  ms/op
+     *
+     * parallelMergeSort usually runs out of memory :(
+     * </pre>
+     */
+
     private static final List<Integer> integers;
 
     static {
         integers = new ArrayList<>();
-        IntStream.range(0, 1000).boxed().forEach(integers::add);
+        IntStream.range(0, 100_000).boxed().forEach(integers::add);
         Collections.shuffle(integers);
     }
 
     @Benchmark
-    public void benchmarkQuickSort(Blackhole bh) {
+    public List<Integer> quicksort(Blackhole bh) {
         var lst = new ArrayList<>(integers);
         SortingAlgorithm<Integer> sorter = new QuickSort<>();
         sorter.sort(lst);
-        bh.consume(lst.size());
+        return lst;
     }
 
     @Benchmark
-    public void benchmarkMergeSort(Blackhole bh) {
+    public List<Integer> mergesort(Blackhole bh) {
         var lst = new ArrayList<>(integers);
         SortingAlgorithm<Integer> sorter = new MergeSort<>();
         sorter.sort(lst);
-        bh.consume(lst.size());
+        return lst;
     }
 
     @Threads(Threads.MAX)
     @Benchmark
-    public void benchmarkParallelMergeSort(Blackhole bh) {
+    public List<Integer> parallelMergesort(Blackhole bh) {
         var lst = new ArrayList<>(integers);
         SortingAlgorithm<Integer> sorter = new ParallelMergeSort<>();
         sorter.sort(lst);
-        bh.consume(lst.size());
+        return lst;
     }
 
     @Benchmark
-    public void benchmarkHeapSort(Blackhole bh) {
+    public List<Integer> heapsort(Blackhole bh) {
         var lst = new ArrayList<>(integers);
         SortingAlgorithm<Integer> sorter = new HeapSort<>();
         sorter.sort(lst);
-        bh.consume(lst.size());
+        return lst;
+    }
+
+    @Benchmark
+    public List<Integer> classicSort(Blackhole bh) {
+        var lst = new ArrayList<>(integers);
+        Collections.sort(lst);
+        return lst;
+    }
+
+    @Benchmark
+    public List<Integer> standardSort(Blackhole bh) {
+        return integers.stream().sorted().toList();
+    }
+
+    @Benchmark
+    public List<Integer> parallelStandardSort(Blackhole bh) {
+        return integers.parallelStream().sorted().toList();
     }
 }
