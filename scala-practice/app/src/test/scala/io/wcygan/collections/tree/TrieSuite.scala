00037070bf496020dc04c5b4aff1df249c72ab8e
Will Cygan <wcygan.io@gmail.com>
2022-04-04 08:26:05 -0500

    more Trie operations

--- scala-practice/app/src/test/scala/io/wcygan/collections/tree/TrieSuite.scala
+++ scala-practice/app/src/test/scala/io/wcygan/collections/tree/TrieSuite.scala
@@ -25,4 +25,56 @@ class TrieSuite extends AnyFunSuite {
     t.add("He")
     assert(t.contains("He"))
   }
+
+  test("Trie prefix indices example 1") {
+    val t = new Trie()
+    t.add("Hi")
+    t.add("Hill")
+    val indices = t.prefixMatchingIndices("Hillbilly")
+    assert(indices.contains(2))
+    assert(indices.contains(4))
+    assert(!indices.contains(1))
+    assert(!indices.contains(3))
+    assert(!indices.contains(5))
+  }
+
+  test("Trie prefix strings example 1") {
+    val t = new Trie()
+    t.add("Hi")
+    t.add("Hill")
+    val strings  = t.prefixMatchingStrings("Hillbilly")
+    assert(strings.contains("Hi"))
+    assert(strings.contains("Hill"))
+    assert(!strings.contains("Hillbilly"))
+  }
+
+  test("Trie strings matching prefix example 1") {
+    val t = new Trie()
+    t.add("Hi")
+    t.add("Hill")
+    val strings  = t.stringsMatchingPrefix("H")
+    assert(strings.contains("Hi"))
+    assert(strings.contains("Hill"))
+    assert(!strings.contains("H"))
+    assert(!strings.contains("Hillbilly"))
+  }
+
+  test("Trie strings matching prefix example 2") {
+    val t = new Trie()
+    t.add("Hello World!")
+    t.add("Hello Will!")
+    t.add("Hello!")
+    t.add("Hello Wally!")
+    val s1  = t.stringsMatchingPrefix("H")
+    assert(s1.contains("Hello!"))
+    assert(s1.contains("Hello World!"))
+    assert(s1.contains("Hello Will!"))
+    assert(s1.contains("Hello Wally!"))
+
+    val s2  = t.stringsMatchingPrefix("Hello W")
+    assert(!s2.contains("Hello!"))
+    assert(s2.contains("Hello World!"))
+    assert(s2.contains("Hello Will!"))
+    assert(s2.contains("Hello Wally!"))
+  }
 }
