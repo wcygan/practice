36de445c0b0e88310f4c4f67e5d048da63fab75c
Will Cygan <wcygan.io@gmail.com>
2022-04-22 23:49:47 -0500

    semaphore and countdownlatch example

---
+++ java-practice/src/main/java/io/wcygan/concurrent/examples/synchronizer/CountDownLatchExample.java
@@ -0,0 +1,63 @@
+package io.wcygan.concurrent.examples.synchronizer;
+
+import io.wcygan.concurrent.executors.ThreadPoolExecutorFactory;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * We can imagine that there are race cars waiting for a race to begin...
+ *
+ * The race begins when all race cars signal that they are ready!
+ *
+ * <pre>
+ *
+ *                            ____________________
+ *                      //|           |        \
+ *                    //  |           |          \
+ *       ___________//____|___________|__________()\__________________
+ *     /__________________|_=_________|_=___________|_________________{}
+ *     [           ______ |           | .           | ==  ______      { }
+ *   __[__        /##  ##\|           |             |    /##  ##\    _{# }_
+ *  {_____)______|##    ##|___________|_____________|___|##    ##|__(______}
+ *              /  ##__##                              /  ##__##        \
+ *
+ * </pre>
+ */
+public class CountDownLatchExample {
+    private static Integer NUM_RACE_CARS = 10;
+
+    public static void main(String[] args) {
+
+        /*
+           The CountDownLatch will wait until its count reaches 0 before unblocking any waiting threads
+        */
+        CountDownLatch signal = new CountDownLatch(NUM_RACE_CARS);
+
+        var tp = ThreadPoolExecutorFactory.create(NUM_RACE_CARS);
+
+        for (int i = 0; i < NUM_RACE_CARS; i++) {
+            tp.execute(prepareRaceCar(i, signal));
+        }
+
+        tp.shutdown();
+    }
+
+    static Runnable prepareRaceCar(int id, CountDownLatch signal) {
+        return () -> {
+            try {
+                // signal that we are ready to race
+                signal.countDown();
+                System.out.println("Racecar " + id + " is waiting!");
+
+                // wait for every other race car to signal that they are ready (beware of deadlock here!)
+                signal.await();
+
+                // race!
+                Thread.sleep(50);
+                System.out.println("Racecar " + id + " is racing!!");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        };
+    }
+}
