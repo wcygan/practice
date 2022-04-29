6970a0663e7cb5d6dd12a6973b5a84256bd7d6c1
Will Cygan <wcygan.io@gmail.com>
2022-04-28 23:26:08 -0500

    zig zag traversal

---
+++ java-practice/src/main/java/io/wcygan/questions/leetcode/medium/q103ZigZag/Solution.java
@@ -0,0 +1,56 @@
+package io.wcygan.questions.leetcode.medium.q103ZigZag;
+
+import io.wcygan.questions.leetcode.TreeNode;
+
+import java.util.*;
+
+// https://leetcode.com/problems/binary-tree-zigzag-level-order-traversal/
+class Solution {
+
+    static class LevelNode {
+        TreeNode node;
+        int level;
+
+        public LevelNode(TreeNode node, int level) {
+            this.node = node;
+            this.level = level;
+        }
+    }
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> lists = new ArrayList<>();
+
+        if (root == null) {
+            return lists;
+        }
+
+        Queue<LevelNode> q = new ArrayDeque<>();
+        q.add(new LevelNode(root, 0));
+
+        while (!q.isEmpty()) {
+            LevelNode curr = q.poll();
+
+            if (curr.node.left != null) {
+                q.add(new LevelNode(curr.node.left, curr.level + 1));
+            }
+
+            if (curr.node.right != null) {
+                q.add(new LevelNode(curr.node.right, curr.level + 1));
+            }
+
+            if (lists.size() != curr.level + 1) {
+                lists.add(new ArrayList<>());
+            }
+
+            lists.get(curr.level).add(curr.node.val);
+        }
+
+        for (int i = 0; i < lists.size(); i++) {
+            if (i % 2 != 0) {
+                Collections.reverse(lists.get(i));
+            }
+        }
+
+        return lists;
+    }
+}
